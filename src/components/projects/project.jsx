9a70f9cf5d9dba08a1229bbd19e699af55a25343
ayoussef <youssef.ammari@ynov.com>
2024-09-27 10:33:50 +0200

    update

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,24 +6,15 @@ import { useAuth } from "../../context/AuthContext"; // Importer le contexte d'a
 import "./styles/project.css";
 
 const Project = (props) => {
-<<<<<<< HEAD
-    const { id, logo, title, description, linkText } = props;
-    const { isAuthenticated } = useAuth(); // Utilise le contexte d'authentification
-
-    // Fonction pour supprimer le projet
-    const handleDelete = (id) => {
-		window.location.href = "http://localhost:8080/delete?"+id;
-=======
     const { logo, title, description, linkText, link } = props;
     const { isAuthenticated } = useAuth(); // Accéder à isAuthenticated depuis le contexte
 
     const handleDelete = () => {
         alert(`Le projet "${title}" a été supprimé.`);
->>>>>>> 3f87399be75fa9173f62d3ffff6f7ab5adafb581
     };
 
     const handleEdit = () => {
-		window.location.href = "/edit";
+        alert(`Modification du projet "${title}".`);
     };
 
     return (
@@ -34,18 +25,6 @@ const Project = (props) => {
                         <div className="project-logo">
                             <img src={logo} alt="logo" />
                         </div>
-<<<<<<< HEAD
-                        <div className="project-link-text">{linkText}</div>
-                    </div>
-                    {isAuthenticated && ( // Remplace IsConnected par isAuthenticated
-                        <div className="project-actions">
-                            <button onClick={handleEdit}>
-                                <FontAwesomeIcon icon={faEdit} />
-                            </button>
-                            <button onClick={handleDelete(id)}>
-                                <FontAwesomeIcon icon={faTrash} />
-                            </button>
-=======
                         <div className="project-title">{title}</div>
                         <div className="project-description">{description}</div>
                         <div className="project-link">
@@ -53,7 +32,6 @@ const Project = (props) => {
                                 <FontAwesomeIcon icon={faLink} />
                             </div>
                             <div className="project-link-text">{linkText}</div>
->>>>>>> 3f87399be75fa9173f62d3ffff6f7ab5adafb581
                         </div>
                         {isAuthenticated && ( // Vérifier si l'utilisateur est connecté
                             <div className="project-actions">
