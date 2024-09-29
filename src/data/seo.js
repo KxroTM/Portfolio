18ac1ba3ef61b11d8f8487b20ab0f4ddf982d8e2
EliasElmir <150239298+EliasElmir@users.noreply.github.com>
2024-09-29 21:21:16 +0200

    update

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,63 +2,63 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			"Je m'appelle Abdellah, j'ai 20 ans et je suis actuellement en deuxième année d'informatique. Passionné par les nouvelles technologies, j'ai déjà acquis une bonne maîtrise des langages comme Python, C, Golang, Java, ainsi que des notions en développement web avec HTML/CSS et JavaScript. Durant mes études, j'ai eu l'occasion de travailler sur plusieurs projets, comme la création d'une application de gestion de bibliothèque en Python et d'un site web personnel pour présenter mes compétences. Je m'intéresse particulièrement à la cybersécurité et à l'intelligence artificielle, domaines dans lesquels j'aimerais me spécialiser à l'avenir. Mon objectif est de devenir développeur logiciel et de continuer à apprendre et à évoluer dans ce secteur passionnant.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			"Passionné par l'informatique depuis plusieurs années, je suis actuellement en deuxième année d'études dans ce domaine. Curieux et motivé, j'aime explorer de nouvelles technologies et relever des défis techniques. Je suis à l'aise avec plusieurs langages de programmation comme Python, Java, Golang,  et C, et je développe régulièrement des projets personnels pour améliorer mes compétences. Mon intérêt se porte particulièrement sur la cybersécurité et l'intelligence artificielle, deux domaines que je souhaite approfondir dans le futur. En parallèle de mes études, je cherche à élargir mes connaissances et à rester à la pointe des innovations technologiques.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "projects",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+			"BY ELIAS ELMIR, YOUSSEF AMMARI, GIOVANNI MASCARO, ILYES ZEKRI, JULIA GARRY.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "contact",
 		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			"STV me contacter passe par feneu.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "admin",
 		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			"Je suis le seul qui peux me conncter car je suis ABDELLAH.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "login",
 		description:
-			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			"J'ai pas de blague avec login donc sa permet juste de se conncter pour cree un nouveaux projets.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "register",
 		description:
-			"fgre",
+			"La page des registre ou tout les élève de Poudlard",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "logout",
 		description:
-			"fgre",
+			"La page de déconnexion.",
 		keywords: ["OOTD"],
 	},
 
 	{
 		page: "404",
 		description:
-			"fgre",
+			"Oh, Noes! A 404!",
 		keywords: ["OOTD"],
 	},
 
